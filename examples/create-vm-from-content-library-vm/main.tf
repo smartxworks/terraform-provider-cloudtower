3b435841b9b4d509816dede871784a6faebb1d94
Shenwen Yin <shenwen.yin@smartx.com>
2022-07-14 16:59:06 +0800

    [doc] update examples of content library template

--- examples/create-vm-from-content-library-vm/main.tf
+++ examples/create-vm-from-content-library-vm/main.tf
@@ -15,35 +15,75 @@ locals {
 
 provider "cloudtower" {
   username          = "root"
-  password          = "111111"
+  password          = "HC!r0cks"
+  user_source       = "LOCAL"
   cloudtower_server = var.config["tower_server"]
 }
 
 data "cloudtower_cluster" "query_clusters" {
-  name = var.config["cluster"]
+  name_in = var.config["cluster"]
 }
 
 data "cloudtower_host" "query_hosts" {
-  management_ip = var.config["host"]
+  management_ip_in = var.config["host"]
 }
 
 data "cloudtower_content_library_vm_template" "query_templates" {
-  name       = var.config["template_name"]
+  name = var.config["template_name"]
 }
 
 data "cloudtower_vlan" "query_vlans" {
-  name = var.config["portroup"]
+  name_in = var.config["portgroup"]
+}
+
+locals {
+  cluster_name_map = {
+    for cluster in data.cloudtower_cluster.query_clusters.clusters :
+    cluster.name => {
+      for k, v in cluster : k => v
+    }
+  }
+  host_ip_map = {
+    for host in data.cloudtower_host.query_hosts.hosts :
+    host.management_ip => {
+      for k, v in host : k => v
+    }
+  }
+  vlan_name_map = { for n, vlans in {
+    for vlan in data.cloudtower_vlan.query_vlans.vlans :
+    vlan.name => vlan...
+    } :
+    n => {
+      for vlan in vlans :
+      vlan.cluster_id => {
+        for k, v in vlan : k => v
+      }
+    }
+  }
+}
+
+output "cluster_name_map" {
+  value = local.vlan_name_map
+}
+
+output "host_ip_map" {
+  value = local.host_ip_map
+}
+
+output "vlan_name_map" {
+  value = local.vlan_name_map
 }
 
 
 resource "cloudtower_vm" "vms_create_from_template" {
-  cluster_id = data.cloudtower_cluster.query_clusters.clusters[0].id
-  count  = length(var.config["vm_ip"])
-  name   = var.config["vm_name"][count.index]
-  memory = var.config["memory"][count.index] * local.MB
-  vcpu   = var.config["vcpu"][count.index]
+  count      = length(var.config["vm_ip"])
+  cluster_id = local.cluster_name_map[var.config["cluster"][count.index]].id #data.cloudtower_cluster.query_clusters.clusters[0].id
+  host_id    = local.host_ip_map[var.config["host"][count.index]].id         #data.cloudtower_host.query_hosts.hosts[0].id
+  name       = var.config["vm_name"][count.index]
+  memory     = var.config["memory"][count.index] * local.MB
+  vcpu       = var.config["vcpu"][count.index]
   create_effect {
-    is_full_copy        = false
+    is_full_copy                        = false
     clone_from_content_library_template = data.cloudtower_content_library_vm_template.query_templates.content_library_vm_templates[0].id
     cloud_init {
       hostname              = var.config["host_name"][count.index]
@@ -53,9 +93,9 @@ resource "cloudtower_vm" "vms_create_from_template" {
         type       = "IPV4"
         nic_index  = 0
         ip_address = var.config["vm_ip"][count.index]
-        netmask    = cidrnetmask("${var.config["vm_ip"][count.index]}/${var.config["cidr"]}")
+        netmask    = cidrnetmask("${var.config["vm_ip"][count.index]}/${var.config["cidr"][count.index]}")
         routes {
-          gateway = var.config["default_gateway"]
+          gateway = var.config["default_gateway"][count.index]
           netmask = "0.0.0.0"
           network = "0.0.0.0"
         }
@@ -88,6 +128,6 @@ resource "cloudtower_vm" "vms_create_from_template" {
     }
   }
   nic {
-    vlan_id = data.cloudtower_vlan.query_vlans.vlans[0].id
+    vlan_id = local.vlan_name_map[var.config["portgroup"][count.index]][local.cluster_name_map[var.config["cluster"][count.index]].id].id #data.cloudtower_vlan.query_vlans.vlans[0].id
   }
 }
